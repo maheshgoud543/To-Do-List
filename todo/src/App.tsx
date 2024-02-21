efde2d89bc44fc75ebb832690aac263d365845d2
maheshgoud543 <goudsmahesh123@gmail.com>
2024-02-21 15:19:18 +0530

    Added Some css and Text

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,7 +1,9 @@
-function App ( ) {
-  return(
-    <div>
-  Hello World
+import "./App.css"
+
+function App() {
+  return (
+    <div className="title_name">
+     <h2>To Do List</h2>
     </div>
   )
 }
